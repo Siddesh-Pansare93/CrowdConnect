b980b52ab4b1b84f66fb3363205b8d188d1774ad
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-09-27 00:21:24 +0530

    added Storage Service for File handling

---
+++ Appwrite/storageService.js
@@ -0,0 +1,73 @@
+import { Storage, Client, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class StorageService {
+    client = new Client()
+    storage
+
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+
+        this.storage = new Storage(Client)
+    }
+
+    // uploding image or any file on the appwrite storage 
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log(`Error :: Appwrite :: UploadFile :: ${error.message}`)
+        }
+    }
+
+    // deleting the file 
+    async deleteFile(fileId){
+        try {
+            return await this.storage.deleteFile(
+                conf.appwriteBucketId ,
+                fileId
+            )
+        } catch (error) {
+            console.log(`Error :: Appwrite :: DeleteFile :: ${error.message}`)   
+        }
+    }
+
+    //updating the file 
+    async updateFile(fileId){
+        try {
+            return await this.storage.updateFile(
+                conf.appwriteBucketId ,
+                fileId
+            )
+        } catch (error) {
+            console.log(`Error :: Appwrite :: UpdateFile :: ${error.message}`)   
+        }
+    }
+
+    // getting file preview so that it can be shown to users
+    async getFilePreview (fileId){
+        try {
+            return  this.storage.getFilePreview(
+                conf.appwriteBucketId ,
+                fileId,
+                
+
+            )
+        } catch (error) {
+            console.log(`Error :: Appwrite :: getFilePreview :: ${error.message}`)   
+            
+        }
+    }
+}
+
+const storageService = new StorageService()
+
+export default storageService
+

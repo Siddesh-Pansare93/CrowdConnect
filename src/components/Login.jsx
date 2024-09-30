8d3a17500bc9238a8c0876b83b8d704daeb18b20
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-09-30 20:45:46 +0530

    login component me logic likha login ka logic likha

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import authService from '@/Appwrite/auth';
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login as storeLogin } from '@/store/Features/authSlice';
 
 function Login() {
+
+  const [error , setError ]= useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogin = async ({ data }) => {
+    setError('')
+    // login logic here
+    try {
+      console.log("Login button clicked");
+      const user = await  authService.login({ ...data })
+      if (user) {
+        const userData = await authService.getCurrentUser()
+        if (userData) {
+          console.log(userData)
+          dispatch(storeLogin(userData))
+          navigate("/")
+        }
+      }
+    }catch (error) {
+          console.log(error.message)
+          navigate("/")
+        }
+      }
+    
+    
+
   return (
-    <div>Login</div>
+    <div>Login </div>
   )
 }
 

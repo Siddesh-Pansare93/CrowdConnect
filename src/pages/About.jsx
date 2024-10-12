7ffff4b47760d8b3af19a6295bfe1a690b9dfdf6
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-12 19:36:23 +0530

    Updated.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -71,7 +71,7 @@ const About = () => {
     return (
         <div className="bg-gray-100 min-h-screen">
             {/* Header Section */}
-            <header className="relative h-96 bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')" }}>
+            <header className="relative h-96 bg-cover bg-center " style={{ backgroundImage: "url('https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')" }}>
                 <div className="absolute inset-0 bg-black opacity-50"></div>
                 <div className="absolute inset-0 flex items-center justify-center">
                     <h1 className="text-5xl font-bold text-white">CrowdConnect</h1>

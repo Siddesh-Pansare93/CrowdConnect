6b8997e1741086fd77a43e2525fa97cb2d549b39
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-13 19:51:44 +0530

    updated package files and tailwind file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,17 @@ export default {
 			},
 			backgroundImage: {
 				"login-pattern": "url('/src/assets/herobg.png')",
-			  },
-			  colors: {
+			},
+			colors: {
 				primary: "#050816",
 				secondary: "#aaa6c3",
 				tertiary: "#151030",
-				bg : "#402C78" ,
-				bg2 : "#ab90f5" ,
+				bg: "#402C78",
+				bg2: "#ab90f5",
 				"black-100": "#100d25",
 				"black-200": "#090325",
 				"white-100": "#f3f3f3",
-			  },
+			},
 		}
 	},
 	plugins: [require("tailwindcss-animate")],

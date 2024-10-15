d4d8a8aecd75112f7b56bef78b1445eaf268cddb
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-15 21:28:31 +0530

    All event page , erros fixed

--- src/components/allEvent.jsx
+++ src/components/allEvent.jsx
@@ -42,10 +42,10 @@ const Events = () => {
 
 
     return (
-        <div className="container  px-4 py-8 bg-white    w-vh">
+        <div className="container  px-4 bg-white    w-vh">
+            <h1 className="text-4xl font-bold mb-8 text-center">Latest Events</h1>
             <SearchBox/>
 
-            <h1 className="text-4xl font-bold mb-8 text-center">Latest Events</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {events.map((event) => (
                     <EventCard key={event.id} event={event} />

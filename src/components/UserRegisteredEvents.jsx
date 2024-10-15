34f72101d07a058195fea3511926d6a857841c3d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-10-15 23:06:55 +0530

    solved error

--- src/components/UserRegisteredEvents.jsx
+++ src/components/UserRegisteredEvents.jsx
@@ -26,15 +26,15 @@ const RegisteredEvents = () => {
     return (
         <div className="bg-black-200 min-h-screen p-6">
             <h2 className="text-3xl font-bold text-white mb-6 text-center">Your Registered Events</h2>
-            {registeredEvents.length > 0 ? (
+            {registeredEvents?.length > 0 ? (
                 <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {registeredEvents.map(event => (
                         <li key={event.$id} className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-md border border-white rounded-3xl p-4 shadow-2xl transition-transform transform hover:scale-105 hover:bg-bg2 hover:bg-opacity-30">
-                            <h3 className="text-xl font-semibold text-white mb-2">{event.eventTitle}</h3>
-                            <p className="text-secondary mb-2">{event.description}</p>
-                            <p className="text-white"><strong>Date:</strong> {event.date}</p>
-                            <p className="text-white"><strong>Location:</strong> {event.location}</p>
-                            <Link to={`/event/${event.$id}`}>
+                            <h3 className="text-xl font-semibold text-white mb-2">{event?.eventTitle || "event Title"}</h3>
+                            <p className="text-secondary mb-2">{event?.description}</p>
+                            <p className="text-white"><strong>Date:</strong> {event?.date}</p>
+                            <p className="text-white"><strong>Location:</strong> {event?.location}</p>
+                            <Link to={`/event/${event?.$id}`}>
                                 <button className='mt-4 bg-blue-800   text-white py-2 px-4 rounded-xl shadow-md hover:bg-secondary transition duration-300'>
                                     View Event Details
                                 </button>

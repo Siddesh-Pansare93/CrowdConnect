d54af587e179bc8aaeb7d0783eb20ced4dea8e49
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-17 23:27:41 +0530

    ..

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,4 +1,3 @@
-// YourEventCard.js
 import { useEffect, useState } from "react";
 import { FaHeart, FaCalendarAlt, FaUsers, FaTicketAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -18,17 +17,14 @@ const YourEventCard = ({ event }) => {
         };
         fetchUser();
     }, [event.organiser]);
-    // CSS for the  card
 
     return (
         <div className="bg-gray-800 rounded-2xl shadow-2xl overflow-hidden transition-transform duration-300 hover:scale-95 p-6 border-5 h-fit">
-
             <img
                 src={storageService.getFilePreview(event.featuredImage)}
                 alt={event.eventTitle}
                 className="w-full h-48 object-cover rounded-xl"
             />
-
             <h2 className="text-xl text-white font-bold mt-4 mb-2">{event.eventTitle}</h2>
             <p className="text-sm text-white mb-4">Hosted by: {user}</p>
 
@@ -42,6 +38,10 @@ const YourEventCard = ({ event }) => {
                 <span className="mx-2">|</span>
                 <FaTicketAlt className="mr-1" />
                 <span>{event.ticketType}</span>
+                {/* Conditionally display ticket price if the type is paid */}
+                {event.ticketType === 'paid' && (
+                    <span className="ml-2 text-yellow-400">₹{event.ticketPrice}</span>
+                )}
             </div>
 
             <p className="text-white mt-2">{event.description || "Join us for an insightful meetup!"}</p>
